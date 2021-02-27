7f316e50db8a93c6a4c4dd54de72bffe80492d63
Sebastian Morawitz <79112911+sepchainer@users.noreply.github.com>
2021-02-27 10:38:33 +0100

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,42 @@
+#include "person.h"
+#include <stdexcept>
+
+Person::Person(const double& groesse, const double& gewicht, const double& alter, const double& iq) : groesse{groesse},
+																						 gewicht{gewicht},
+																						 alter{ alter },
+																						 iq{ iq } 
+{
+	if (groesse <= 1.0)
+		throw std::runtime_error("Die eingegebene Groeße ist zu gering");
+	if (gewicht <= 40.0)
+		throw std::runtime_error("Das eingegebene Gewicht ist zu gering");
+	if (alter <= 18.0)
+		throw std::runtime_error("Es werden nur volljährige Personen eingerechnet");
+	if (iq <= 90.0)
+		throw std::runtime_error("Menschen mit solch einem IQ koennen nicht leben");
+}
+
+double Person::get_groesse() const {
+	return groesse;
+}
+
+double Person::get_gewicht() const {
+	return gewicht;
+}
+
+double Person::get_alter() const {
+	return alter;
+}
+
+double Person::get_iq() const {
+	return iq;
+}
+
+
+std::ostream& Person::print(std::ostream& o) const {
+	return o << "Alter: " << alter << "; Groesse: " << groesse << "; Gewicht: " << gewicht << "; IQ: " << iq << ";";
+}
+
+std::ostream& operator<<(std::ostream& o, const Person& p) {
+	return p.print(o);
+}
